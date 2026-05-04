46618919ea05614ce2136c234caf47eea2024455
Raúl <raul.fernandez23@estudiantes.uva.es>
2026-05-04 22:47:25 +0200

    payment service env fix

--- servidor/src/services/payment.service.ts
+++ servidor/src/services/payment.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: process.env.WEBSERVICES_BASE_URL || 'https://webservices.samuelencinas.dev/api',
+    baseURL: process.env.API_BASE_URL || "api_falsa_para_que_typescript_no_de_error.com",
     timeout: 5000,
     headers: { 'Content-Type': 'application/json'}
 });
@@ -9,8 +9,8 @@ const client = axios.create({
 export const PaymentService = {
     login: async (): Promise<string> => {
         const { data } = await client.post('/auth/login', {
-            username: process.env.WEBSERVICES_USERNAME,
-            password: process.env.WEBSERVICES_PASSWORD
+            username: process.env.API_AUTH_USER,
+            password: process.env.API_AUTH_PASSWORD
         });
         return data.access_token;
     },
